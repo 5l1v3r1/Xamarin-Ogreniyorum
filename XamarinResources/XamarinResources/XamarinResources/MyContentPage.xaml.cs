d6bde9ed0efcf1d87508af0812674284f466cc30
Yiğit <ozaksuty@gmail.com>
2016-06-05 18:49:41 +0300

    Xamarin Forms Dynamic Resources ve Theme Mantığı
    Xamarin Forms Dynamic Resources ve Theme Mantığı

---
+++ XamarinResources/XamarinResources/XamarinResources/MyContentPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinResources
+{
+    public partial class MyContentPage : ContentPage
+    {
+        public MyContentPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnLight(object sender, EventArgs e)
+        {
+            Resources["fgColor"] = Color.FromHex("#606060");
+            Resources["bgColor"] = Color.FromHex("#c0c0c0");
+        }
+
+        void OnDark(object sender, EventArgs e)
+        {
+            Resources["fgColor"] = Color.FromHex("#c0c0c0");
+            Resources["bgColor"] = Color.FromHex("#606060");
+        }
+    }
+}
