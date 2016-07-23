c19771acdfe0fced20b4ac094e48b2f6da07f2d5
Yiğit <ozaksuty@gmail.com>
2016-07-23 20:47:38 +0300

    Xamarin GestureRecognizer and Animation
    Xamarin GestureRecognizer and Animation

---
+++ LiveCodingAnimation/LiveCodingAnimation/LiveCodingAnimation/AnimationPage.cs
@@ -0,0 +1,51 @@
+﻿using Xamarin.Forms;
+
+namespace LiveCodingAnimation
+{
+    public class AnimationPage : ContentPage
+    {
+        public AnimationPage()
+        {
+            this.BackgroundColor = Color.White;
+
+            RelativeLayout layout = new RelativeLayout();
+
+            Image cc = new Image
+            {
+                Source = "visa.png",
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            cc.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(async (t) =>
+                {
+                    bool isOK = await DisplayAlert("Animasyon", "Calışsın mı?",
+                        "OK", "CANCEL");
+                    if (isOK)
+                    {
+                        await cc.ScaleTo(0.95, 100, Easing.CubicOut);
+                        await cc.ScaleTo(1, 50, Easing.CubicIn);
+                        await cc.LayoutTo(new Rectangle(0, 150,
+                            cc.Width, cc.Height), 1250, Easing.SpringIn);
+                        await cc.RotateYTo(180, 1250, Easing.Linear);
+                        await cc.FadeTo(.5, 1000, Easing.BounceOut);
+                        await cc.TranslateTo((cc.Width / 2), (cc.Height / 2),
+                            500, Easing.SinIn);
+                    }
+                })
+            });
+
+            layout.Children.Add(cc, Constraint.Constant(0),
+                Constraint.RelativeToParent((parent =>
+                {
+                    return parent.Height - 50;
+                })), Constraint.RelativeToParent((parent =>
+                {
+                    return parent.Width;
+                })));
+
+            Content = layout;
+        }
+    }
+}
---
+++ XamarinAnimation/XamarinAnimation/XamarinAnimation/AnimationPage.cs
@@ -0,0 +1,45 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinAnimation
+{
+    public class AnimationPage : ContentPage
+    {
+        public AnimationPage()
+        {
+            this.BackgroundColor = Color.White;
+            
+            RelativeLayout layout = new RelativeLayout();
+
+            Image cc = new Image
+            {
+                Source = "visa.png",
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            cc.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(async (t) =>
+                {
+                    await DisplayAlert("Title", "Message", "OK");
+                    //await cc.ScaleTo(0.95, 100, Easing.CubicOut);
+                    //await cc.ScaleTo(1, 50, Easing.CubicIn);
+                    //await cc.LayoutTo(new Rectangle(0, 150, cc.Width, cc.Height), 1250, Easing.SpringIn);
+                    //await cc.RotateYTo(Device.OnPlatform(179, 180, 180), 1250, Easing.BounceIn);
+                })
+            });
+
+            layout.Children.Add(cc, Constraint.Constant(0),
+                Constraint.RelativeToParent((parent =>
+                {
+                    return parent.Height - 50;
+                })),
+                Constraint.RelativeToParent((parent =>
+                {
+                    return parent.Width;
+                }))
+            );
+
+            Content = layout;
+        }
+    }
+}
