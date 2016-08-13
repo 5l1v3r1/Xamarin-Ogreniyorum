2ab9fb4653e66196f7d0f9c28bb7fd6a621b43b6
Yiğit <ozaksuty@gmail.com>
2016-08-13 16:30:29 +0300

    Xamarin.Forms Media Plugin
    Xamarin.Forms Media Plugin

--- ListViewGrouping/ListViewGrouping/ListViewGrouping/Data/ImageResourceConverter.cs
+++ ListViewGrouping/ListViewGrouping/ListViewGrouping/Data/ImageResourceConverter.cs
@@ -5,12 +5,15 @@ namespace ListViewGrouping.Data
 {
     public class ImageResourceConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, 
+            object parameter, System.Globalization.CultureInfo culture)
         {
-            return ImageSource.FromResource("ListViewGrouping.Images.Player." + (value ?? ""));
+            return ImageSource.FromResource("ListViewGrouping.Images.Player." 
+                + (value ?? ""));
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, 
+            object parameter, System.Globalization.CultureInfo culture)
         {
             throw new NotSupportedException();
         }
---
+++ MediaPlugin/MediaPlugin/MediaPlugin.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("MediaPlugin.Droid.MainApplication, MediaPlugin.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5167d52c4af08698df1ca16910594c6d9.MainApplication.class, md5167d52c4af08698df1ca16910594c6d9.MainApplication.__md_methods);
+		
+	}
+}
