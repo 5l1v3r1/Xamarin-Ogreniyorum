e13fd9071b5ee7c1bb13a135d1d9bee1c143111c
Yiğit <ozaksuty@gmail.com>
2016-09-06 22:57:48 +0300

    Xamarin Forms Local Push Notification
    Xamarin Forms Local Push Notification

---
+++ XamarinLocalPush/XamarinLocalPush/XamarinLocalPush/App.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using XamarinLocalPush.Views;
+
+namespace XamarinLocalPush
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new Register();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XamarinLocalPush/XamarinLocalPush/XamarinLocalPush/Views/Register.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Plugin.LocalNotifications;
+using System;
+using Xamarin.Forms;
+
+namespace XamarinLocalPush.Views
+{
+    public partial class Register : ContentPage
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void OnInsert(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string surname = txtSurname.Text;
+            CrossLocalNotifications.Current.Show("Xamarin Türkiye",
+                name + " " + surname, 1, DateTime.Now.AddSeconds(30));
+
+            CrossLocalNotifications.Current.Show("Xamarin Türkiye",
+                "www.xamarintr.com", 2, DateTime.Now.AddSeconds(30));
+
+            CrossLocalNotifications.Current.Cancel(2);
+        }
+    }
+}
