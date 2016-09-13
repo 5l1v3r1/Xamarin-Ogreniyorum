5be3ea8725454ea5782375f4e08ce43d3558f204
Yiğit <ozaksuty@gmail.com>
2016-09-13 18:36:27 +0300

    Xamarin DisplayActionSheet
    Xamarin DisplayActionSheet

---
+++ XamarinDisplayAction/XamarinDisplayAction/XamarinDisplayAction/DisplayPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinDisplayAction
+{
+    public partial class DisplayPage : ContentPage
+    {
+        public DisplayPage()
+        {
+            InitializeComponent();
+
+            Button btnDisplayAction = new Button
+            {
+                Text = "Options",
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            string[] colors = { "red", "blue", "black", "white", "yellow" };
+            btnDisplayAction.Clicked += async (o, s) =>
+            {
+                string selectedColor = await DisplayActionSheet(
+                    "Select Color", "Cancel", "Ok", colors);
+                switch (selectedColor)
+                {
+                    case "red":
+                        this.BackgroundColor = Color.Red;
+                        break;
+                    case "blue":
+                        this.BackgroundColor = Color.Blue;
+                        break;
+                    case "black":
+                        this.BackgroundColor = Color.Black;
+                        break;
+                    case "white":
+                        this.BackgroundColor = Color.White;
+                        break;
+                    case "yellow":
+                        this.BackgroundColor = Color.Yellow;
+                        break;
+                }
+            };
+
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    btnDisplayAction
+                }
+            };
+        }
+    }
+}
