94b841ec16cef885f51be889bd0a8cb704a1d1bb
Yiğit <ozaksuty@gmail.com>
2016-10-08 20:43:15 +0300

    XamarinBarcodeQRReader
    XamarinBarcodeQRReader

---
+++ XamarinReader/XamarinReader/XamarinReader/GeneratePage.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace XamarinReader
+{
+    public class GeneratePage : ContentPage
+    {
+        public GeneratePage()
+        {
+            ZXingBarcodeImageView barcode = new ZXingBarcodeImageView
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                AutomationId = "zxingBarcodeImageView"
+            };
+
+            barcode.BarcodeFormat = ZXing.BarcodeFormat.QR_CODE;
+            barcode.BarcodeValue = "http://www.xamarintr.com";
+
+            Content = barcode;
+        }
+    }
+}
---
+++ XamarinReader/XamarinReader/XamarinReader/ReaderPage.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using ZXing;
+using ZXing.Mobile;
+using ZXing.Net.Mobile.Forms;
+
+namespace XamarinReader
+{
+    public class ReaderPage : ContentPage
+    {
+        ZXingScannerPage sPage;
+        public ReaderPage()
+        {
+            sPage = new ZXingScannerPage(new MobileBarcodeScanningOptions
+            {
+                AutoRotate = true,
+                PossibleFormats = new List<BarcodeFormat>
+                {
+                    BarcodeFormat.QR_CODE,
+                    BarcodeFormat.AZTEC
+                },
+                UseFrontCameraIfAvailable = true,
+                UseNativeScanning = true
+            });
+
+            sPage.ToggleTorch();
+            sPage.OnScanResult += SPage_OnScanResult;
+
+            Button scanButton = new Button
+            {
+                Text = "Scan",
+                AutomationId = "scanContinuously"
+            };
+            Button generateButton = new Button
+            {
+                Text = "Generate",
+                AutomationId = "barcodeGenerator"
+            };
+
+            generateButton.Clicked += GenerateButton_Clicked;
+
+            scanButton.Clicked += ScanButton_Clicked;
+
+            var stack = new StackLayout();
+            stack.Children.Add(scanButton);
+            stack.Children.Add(generateButton);
+
+            Content = stack;
+        }
+
+        private async void GenerateButton_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new GeneratePage());
+        }
+
+        private void SPage_OnScanResult(ZXing.Result result)
+        {
+            sPage.IsScanning = false;
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Navigation.PopAsync();
+                DisplayAlert("Scan", result.Text, "Ok");
+            });
+        }
+
+        private async void ScanButton_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(sPage);
+        }
+    }
+}
