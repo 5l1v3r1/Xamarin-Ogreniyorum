3274ff40bd305ed667bcfeccccc810e00fded304
Yiğit <ozaksuty@gmail.com>
2017-01-12 20:51:55 +0300

    HockeyApp
    HockeyApp

---
+++ HockeyApp/HockeyApp/HockeyApp.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace HockeyApp.Droid
+{
+    [Activity(Label = "HockeyApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        private const string AppId = "b43d74cc25f148e0aa172e080e5785d8";
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            HockeyApp.Android.CrashManager.Register(this, AppId);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ HockeyApp/HockeyApp/HockeyApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HockeyApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            this.Title = "Xamarin.Forms - HockeyApp";
+            InitializeComponent();
+        }
+
+        private void onBoom(object sender, EventArgs e)
+        {
+            throw new Exception(); //BOOM! Crash App
+        }
+    }
+}
